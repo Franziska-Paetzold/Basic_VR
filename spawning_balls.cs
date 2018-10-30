ab8e17cf05f8804ae3069a18f16ab00bdeeba6d2
Franziska Pätzold <s0554620@htw-berlin.de>
2018-10-30 11:59:01 +0100

    add spawning balls

---
+++ spawning_balls.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawning_balls : MonoBehaviour {
+
+	public GameObject ballPrefab;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("multiplyBalls", 1.0f, 0.2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void multiplyBalls(){
+		Instantiate (ballPrefab, transform);
+	}
+}
